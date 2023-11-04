9bc194ce3f280c76afb07d1401ebd6beda848a84
Hashan Ruchira <hashanruchira98@gmail.com>
2023-11-05 01:14:52 +0530

    change the default names

--- app/src/main/java/com/example/pulseapp/ui/bookings/BookingsFragment.java
+++ app/src/main/java/com/example/pulseapp/ui/bookings/BookingsFragment.java
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.pulseapp.databinding.FragmentDashboardBinding;
+import com.example.pulseapp.databinding.FragmentBookingsBinding;
 
 public class BookingsFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentBookingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         BookingsViewModel bookingsViewModel =
                 new ViewModelProvider(this).get(BookingsViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentBookingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
+        final TextView textView = binding.textBookings;
         bookingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
--- app/src/main/java/com/example/pulseapp/ui/movies/MoviesFragment.java
+++ app/src/main/java/com/example/pulseapp/ui/movies/MoviesFragment.java
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.pulseapp.databinding.FragmentHomeBinding;
+import com.example.pulseapp.databinding.FragmentMoviesBinding;
 
 public class MoviesFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentMoviesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MoviesViewModel moviesViewModel =
                 new ViewModelProvider(this).get(MoviesViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentMoviesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        final TextView textView = binding.textMovies;
         moviesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
